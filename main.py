470339f4aca1c946f205a485635344d63b5bec92
Dias <kasenov_06@internet.ru>
2026-07-27 16:44:27 +0500

    changed code structure

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from parser import parsing_catalog_1, parsing_catalog_2, parsing_catalog_3
+from parser import parsing_catalog
 print('Крупно-бытовая техника')
 url_1 = 'https://mega-kuhnya.ru/catalog/krupno-bitovaya-tekhnika/'
-parsing_catalog_1(url_1)
+parsing_catalog(url_1)
 print('Встраиваемая техника')
 url_2 = 'https://mega-kuhnya.ru/catalog/vstraivaemaya-tehnika/'
-parsing_catalog_2(url_2)
+parsing_catalog(url_2)
 print('Климатическое оборудование')
 url_3 = 'https://mega-kuhnya.ru/catalog/klimaticheskoe-oborudovanie/'
-parsing_catalog_3(url_3)
+parsing_catalog(url_3)
--- parser.py
+++ parser.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
 headers = {
 
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -7,45 +10,23 @@ headers = {
     "Chrome/120.0 Safari/537.36",
 "Accept-Language": "ru-RU,ru;q=0.9",
 }
+
 def make_session():
     session = requests.Session()
     session.headers.update(headers)
+
+    retry = Retry(
+        total=5,
+        backoff_factor=1,
+        status_forcelist=[429, 500, 502, 503, 504],
+        allowed_methods=["GET"],
+        respect_retry_after_header=True,
+    )
+    session.mount("https://", HTTPAdapter(max_retries=retry))
+    session.mount("http://", HTTPAdapter(max_retries=retry))
     return session
 
-def parsing_catalog_1(url):
-    session = make_session()
-    response = session.get(url, timeout=(3.05, 27))
-
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    catalogs_soup = soup.find_all('div', class_='col-xxl-4 col-lg-3 col-md-4 col-sm-6 col-xs-6 custom_category_grid')
-    catalogs = []
-
-    for catalog in catalogs_soup:
-        catalog_url = catalog.find('a').get('href')
-        catalogs.append(catalog_url)
-
-    for catalog in catalogs:
-        print(catalog)
-
-def parsing_catalog_2(url):
-    session = make_session()
-    response = session.get(url, timeout=(3.05, 27))
-
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    catalogs_soup = soup.find_all('div', class_='col-xxl-4 col-lg-3 col-md-4 col-sm-6 col-xs-6 custom_category_grid')
-    catalogs = []
-
-    for catalog in catalogs_soup:
-        catalog_url = catalog.find('a').get('href')
-        catalogs.append(catalog_url)
-
-    for catalog in catalogs:
-        print(catalog)
-
-
-def parsing_catalog_3(url):
+def parsing_catalog(url):
     session = make_session()
     response = session.get(url, timeout=(3.05, 27))
 
